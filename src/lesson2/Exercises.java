c17679c9f6194e719ca0cd5cb9b20abbc18e2812
Alexander Chernomyrdin <chav1961@mail.ru>
2020-06-25 19:05:11 +0500

    DAily commit

---
+++ src/lesson2/Exercises.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Exercises {
+	static int[]	content = {10,22,-3,17,21,14};
+	
+	public static void main(String[] args) {
+		int	avg = 0;
+		
+		// TODO Calculate average on content
+		
+		System.err.println("Avg="+avg);
+	}
+}
