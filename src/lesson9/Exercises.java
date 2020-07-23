47817dfe903214bd96bc62f0a2766501d11745c1
Alexander Chernomyrdin <chav1961@mail.ru>
2020-07-23 19:49:42 +0500

    Daily commit

--- src/lesson9/Exercises.java
+++ src/lesson9/Exercises.java
@@ -1,14 +1,28 @@
 package lesson9;
 
+import java.lang.reflect.Field;
+
 public class Exercises {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
 		// TODO Auto-generated method stub
 		System.err.println(sum(new Class1()));
 		System.err.println(sum(new Class2()));
 	}
 
-	static int sum(final Object obj) {
+	static int sum(final Object obj) throws IllegalArgumentException, IllegalAccessException {
 		// TODO: calculate sum of all int fields in the object
+		Class	cl = obj.getClass();
+		int		sum = 0;
+
+		do {
+			for (Field f : cl.getDeclaredFields()) {
+				if (f.getType() == int.class) {
+					sum += f.getInt(obj);
+				}
+			}
+		} while ((cl = cl.getSuperclass()) != null);
+		
+		return sum;
 	}
 	
 }
